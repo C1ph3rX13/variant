09086c37dc48522d7436af17a727478778542660
C1ph3rX13 <t-r2x@qq.com>
2024-02-07 13:50:17 +0800

    初始化bat

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo Installing go-wines
+go install github.com/tc-hib/go-winres@latest
+
+echo Installing goimports
+go install golang.org/x/tools/cmd/goimports@latest
+
+echo Installing garble
+go install mvdan.cc/garble@latest
+
+echo Done
+pause
